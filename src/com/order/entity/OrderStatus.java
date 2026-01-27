2c654fcb1ae4c42d40cae781d6b1ec10ed37cbc4
Xiyu2597 <2942856015@qq.com>
2026-01-27 23:26:07 +0800

    OrederStatus Modify

--- src/com/order/entity/OrderStatus.java
+++ src/com/order/entity/OrderStatus.java
@@ -1,5 +1,12 @@
 package com.order.entity;
 
 public enum OrderStatus {
-    CREATED,PROCESSING,PROCESSED
+    CREATED("已创建"),PROCESSING("处理中"),PROCESSED("已完成");
+    private String desc;
+    OrderStatus(String desc) {
+        this.desc = desc;
+    }
+    public String getDesc() {
+        return desc;
+    }
 }
