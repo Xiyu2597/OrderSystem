ce515a80f78ce9fc5adf2ad17d34d01689e1d289
Xiyu2597 <2942856015@qq.com>
2026-02-12 19:11:06 +0800

    test

--- src/File/Main.java
+++ src/File/Main.java
@@ -6,17 +6,9 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
-        FileInputStream stream = null;
-        try {
-            stream = new FileInputStream("test.txt");
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                stream.close();
+        try ( FileInputStream  stream = new FileInputStream("test.txt")){
             }catch (IOException e) {
                 throw new RuntimeException(e);
             }
         }
     }
-}
