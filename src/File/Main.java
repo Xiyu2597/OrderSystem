0403c93ad983839a8f0986abaf4d6cb5f0548b42
Xiyu2597 <2942856015@qq.com>
2026-02-12 19:30:26 +0800

    test

--- src/File/Main.java
+++ src/File/Main.java
@@ -7,8 +7,17 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) {
         try ( FileInputStream  stream = new FileInputStream("test.txt")){
+            byte[] arr = new byte[stream.available()];
+            stream.skip(6);
+            System.out.println(stream.read(arr));
+            System.out.println(new String(arr));
+//            System.out.println(stream.available());
+//         int b;
+//         while((b = stream.read()) !=-1){
+//             System.out.print((char)b);
+//            }
             }catch (IOException e) {
-                throw new RuntimeException(e);
+                e.printStackTrace();
             }
         }
     }
